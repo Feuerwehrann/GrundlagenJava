844ad2aee5738c60e07364ea815736a7f3999157
Ann Manegold <a.manegold@AD.SINGEN-HGS.LOGODIDACT.NET>
2021-12-21 12:57:11 +0100

    Kommentar deledet

--- src/a06_Verzweigungen/Restprüfer.java
+++ src/a06_Verzweigungen/Restprüfer.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Restprüfer {
 	public static void main(String[] args) {
 		//EINGABE
-		//TEST
 		double teiler, prüfZahl;
 		Scanner sc = new Scanner (System.in);
 		
