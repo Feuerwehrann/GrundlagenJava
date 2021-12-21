486404b447c7ce74c0df6f1268a9602cbd24bb8f
Ann Manegold <a.manegold@AD.SINGEN-HGS.LOGODIDACT.NET>
2021-12-21 12:14:52 +0100

    Kommentar added

--- src/a06_Verzweigungen/FIlm_Geburtsdatum_Prüfer_1.java
+++ src/a06_Verzweigungen/FIlm_Geburtsdatum_Prüfer_1.java
@@ -5,7 +5,7 @@ import java.time.Period;
 import java.util.Scanner;
 
 public class FIlm_Geburtsdatum_Prüfer_1 {
-
+  //test
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
 		System.out.print("Geburtsjahr eingeben:\t");
